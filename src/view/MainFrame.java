00266fedc44e5dbecdb1e4afb4ad3deaa955ba48
Haric Silver <Haric Silver@192.168.2.21>
2015-11-04 15:24:36 +0700

    Init project with libs - Swingx all 164 - Weblaf 129 - Form

---
+++ src/view/MainFrame.java
@@ -0,0 +1,44 @@
+package view;
+
+import java.awt.EventQueue;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import com.alee.laf.WebLookAndFeel;
+
+public class MainFrame extends JFrame {
+
+	private static final long serialVersionUID = 8720432252745326845L;
+	private JPanel contentPane;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WebLookAndFeel.install();
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public MainFrame() {
+		setTitle("F2P");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+		setBounds(100, 100, 799, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+	}
+
+}
